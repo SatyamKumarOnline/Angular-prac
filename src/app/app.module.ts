6df0dddd70515f6b4ea4cec79d4587306106e3f4
unknown <registersatyam@gmail.com>
2023-10-04 21:03:15 +0530

    Input two way data binding. For input element, define read and write op using two variables. Output event.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseCardComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
